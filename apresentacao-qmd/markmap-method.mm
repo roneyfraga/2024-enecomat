f1639e5c1565c104b5e14fbd6aa60ca2f2efae2a
Roney Fraga Souza <roneyfraga@gmail.com>
2024-12-03 06:35:05 -0400

    2024-12-03 finalizada

---
+++ apresentacao-qmd/markmap-method.mm
@@ -0,0 +1,60 @@
+---
+markmap:
+  colorFreezeLevel: 2
+  maxWidth: 300
+  initialExpandLevel: 3
+---
+
+# Souza et al.
+
+## **1.** Collect data
+
+- Scopus
+- WoS 
+- Derwent 
+- Google Patents
+
+## **2.** Import and organize the data
+
+- `R` + `bibliometrix` + `others`
+
+## **3.** Field analysis
+
+- Publication over time 
+- Growth rate
+- Predicted publication
+- Categories overview
+
+## **4.** Network analysis `birddog`
+
+### **4.1** Creat a citation network
+
+- Bibliographic coupling, or
+- Direct citation, or
+- Co-citation 
+
+### **4.2** Extract the giant component
+
+### **4.3** Build groups
+
+#### Louvain algorithm or alternative
+
+### **4.4** Groups attributes
+
+#### Size
+#### Average age
+#### Growth rate
+#### Publication over time
+
+### **4.5** Groups contents
+
+#### Natural language procesing - NLP (*rake algorithm*)
+#### Text mining - TM (*tf-idf*) 
+#### Hubs (*topological rules*)
+#### Strutural topic modeling – STM
+
+### **4.6** Groups evolution network
+
+#### Groups trajectories
+
+
